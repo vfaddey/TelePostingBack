ff1b88709238ce28bf8fbcfacfce65b6f0d5ac88
vfaddey <vfaddey@yandex.ru>
2024-07-14 20:44:17 +0300

    добавил нормальное составление постов + инфу запихал в сами кнопки

--- routers/telegram/post_publisher.py
+++ routers/telegram/post_publisher.py
@@ -1,3 +1,4 @@
+from turtle import pos
 from .bot_manager import BotManager
 from bson import ObjectId
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -25,18 +26,58 @@ class PostPublisher:
         self.post_repository = post_repository
 
     async def fetch_post_and_send_message(self, post_id, user_id):
-        markup = InlineKeyboardMarkup(row_width=2)
         post = await self.post_repository.get_post(ObjectId(post_id))
+        channels = post.channels
+        if len(channels) == 0:
+            return
+        photos = post.photo_ids
+        photo_urls = post.photo_urls
+        if photo_urls is None:
+            photo_urls = []
+        try:
+            if len(photos) == 0 and len(photo_urls) == 0:
+                return await self.__send_text_message(post, user_id, channels)
+            elif len(photos) == 1:
+                return await self.__send_photo_message(post, user_id, channels)
+            elif len(photo_urls) == 1:
+                return await self.__send_photo_url_message(post, user_id, channels)
+            elif len(photos) > 1:
+                return await self.__send_media_group(post, user_id, channels)
+        except InvalidPostException:
+            return
+
+    async def __send_text_message(self, post, user_id, channels: list[str]):
+        markup = InlineKeyboardMarkup(row_width=2)
         user = await ausers_collection.find_one({'_id': ObjectId(user_id)})
+        user_channels_usernames = [channel['username'] for channel in user.get('channels', [])]
+        print(user_channels_usernames)
+        if len(user_channels_usernames) == 0:
+            raise InvalidPostException
+        
         if user.get('bots', None):
             for bot in user['bots']:
                 if bot.get('active', None):
                     current_bot = bot_manager.get_bot(bot['api_token'])
                     if post:
+                        posts_in_channels = []
                         if post.buttons:
-                            markup = self.prepare_markup(post.buttons, post_id)
-                        post_in_channel = current_bot.send_message('@unforgetable_facts', post.text, reply_markup=markup)
-                        await self.post_repository.mark_as_posted(ObjectId(post_id))
+                            markup = self.prepare_markup(post.buttons, post.id)
+                        for channel in channels:
+                            if channel in user_channels_usernames:
+                                post_in_channel = current_bot.send_message(channel, post.text, reply_markup=markup)
+                                posts_in_channels.append(post_in_channel)
+                        await self.post_repository.mark_as_posted(ObjectId(post.id))
+                        return post_in_channel
+        raise InvalidPostException
+
+    async def __send_photo_message(self, post, user_id, channels: list[str]):
+        pass
+
+    async def __send_photo_url_message(self, post, user_id, channels: list[str]):
+        pass
+
+    async def __send_media_group(self, post, user_id, channels: list[str]):
+        pass
 
     def prepare_markup(self, buttons: dict, post_id: ObjectId):
         markup = InlineKeyboardMarkup(row_width=2)
@@ -45,6 +86,10 @@ class PostPublisher:
                 url_button = InlineKeyboardButton(button['text'], url=button['url'])
                 markup.add(url_button)
             elif button['type'] == 'text':
-                text_button = InlineKeyboardButton(button['text'], callback_data=f'button_{post_id}_{index}')
+                text_button = InlineKeyboardButton(button['text'], callback_data=f'button_{button["subscriberText"]}_{button["guestText"]}')
                 markup.add(text_button)
         return markup
+
+
+class InvalidPostException(Exception):
+    ...
