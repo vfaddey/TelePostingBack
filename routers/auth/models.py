9e9a46a0707deff99b770bee9cab84404770c133
vfaddey <vfaddey@yandex.ru>
2024-07-21 23:05:13 +0300

    добавил проверку на верифицированность пользователя

--- routers/auth/models.py
+++ routers/auth/models.py
@@ -4,5 +4,8 @@ from pydantic import BaseModel, EmailStr
 class User(BaseModel):
     id: str
     username: str
+    verified: bool
+    telegram_id: int
+    telegram_username: str
     email: EmailStr
     hashed_password: str
