a12a9b586185a481b9f9c3aecea7f2b31091a52d
vfaddey <vfaddey@yandex.ru>
2024-07-27 23:41:41 +0300

    хз.....

--- routers/posts/post_repository.py
+++ routers/posts/post_repository.py
@@ -1,4 +1,5 @@
 from datetime import timezone
+import stat
 
 from pandas import json_normalize
 from .schemas import Post, AddPost
@@ -29,12 +30,21 @@ class PostRepository:
                 raise HTTPException(status_code=400,
                                     detail='Нельзя создавать кнопки, если в альбоме больше одной картинки')
 
+        if add_post.publish_time and add_post.delete_time:
+            if add_post.delete_time > add_post.publish_time:
+                raise HTTPException(status_code=400,
+                                    detail='Дата удаления не может быть раньше даты публикации')
+
+        if (not add_post.publish_now) and (not add_post.publish_time):
+            raise HTTPException(status_code=400,
+                                detail='Необходимо выбрать время публикации')
+
         photo_ids = []
         if add_post.photos:
             photo_ids = await self._add_photos(add_post.photos)
 
         if add_post.publish_time is not None:
-            if add_post.publish_time.tzinfo is None: 
+            if add_post.publish_time.tzinfo is None:
                 add_post.publish_time = add_post.publish_time.replace(tzinfo=timezone.utc)
 
         channel_list = json.loads(add_post.channels)
@@ -60,7 +70,6 @@ class PostRepository:
             post['id'] = str(result.inserted_id)
         return Post(**post)
 
-
     async def _add_photos(self, files: list[UploadFile]) -> list:
         photo_ids = []
         for file in files:
@@ -70,7 +79,7 @@ class PostRepository:
             await grid_in.close()
             photo_ids.append(grid_in._id)
         return photo_ids
-    
+
     async def get_photos(self, photo_ids: list[str]) -> list[BytesIO]:
         photos = []
         if isinstance(photo_ids[0], str):
@@ -81,7 +90,7 @@ class PostRepository:
                 photo_data.extend(chunk)
             photos.append(BytesIO(photo_data))
         return photos
-    
+
     async def get_photo(self, photo_id: str | ObjectId) -> BytesIO:
         photo_data = bytearray()
         if isinstance(photo_id, str):
@@ -89,7 +98,7 @@ class PostRepository:
         async for chunk in await self.fs.open_download_stream(photo_id):
             photo_data.extend(chunk)
         return BytesIO(photo_data)
-    
+
     async def get_posts(self, user_id: str, posted: bool) -> list[Post]:
         post_list = []
         result = self.posts_collection.find({
@@ -103,7 +112,7 @@ class PostRepository:
                 post_list.append(Post(**post))
             return post_list
         raise HTTPException(status_code=400, detail='Не найдена информация о постах')
-    
+
     async def get_post(self, post_id: ObjectId) -> Post:
         result = await self.posts_collection.find_one({'_id': post_id})
         if result:
@@ -115,8 +124,17 @@ class PostRepository:
     async def update_post(self, post_id, post: Post):
         pass
 
-    async def delete_post(self, post_id):
-        pass
+    async def delete_post(self, post_id, user_id):
+        if not self.posts_collection.find_one({"_id": ObjectId(post_id)}):
+            raise HTTPException(status_code=404, detail="Этот пост не найден")
+
+        result = await self.posts_collection.delete_one({
+            "_id": ObjectId(post_id),
+            "owner_id": user_id
+        })
+        if result.deleted_count == 0:
+            raise HTTPException(status_code=401, detail='Вы не являетесь создаталем поста')
+        return True
 
     async def mark_as_posted(self, post_id) -> bool:
         result = await self.posts_collection.update_one({'_id': post_id},
@@ -127,3 +145,9 @@ class PostRepository:
                                                         })
         return result.matched_count > 0
 
+    async def save_message_id(self, post_id: ObjectId, messages: list[dict]):
+        result = await self.posts_collection.update_one({'_id': post_id},
+                                                        {
+                                                            '$set': {'messages': messages}
+                                                        }, upsert=True)
+        return result.modified_count > 0
--- routers/posts/post_service.py
+++ routers/posts/post_service.py
@@ -3,7 +3,7 @@ from fastapi import HTTPException
 from .post_repository import PostRepository
 from redis import Redis
 import threading
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timezone
 from .schemas import Post, AddPost
 from bson import ObjectId
 from routers.telegram.post_publisher import PostPublisher
@@ -20,16 +20,26 @@ class PostService:
     async def add_post(self, add_post: AddPost) -> Post:
         post = await self.post_repository.add_post(add_post)
         if post:
-            if add_post.publish_now:
+            if post.publish_now:
                 await self.publish_now(post.id, post.owner_id)
             else:
-                await self.broker.set(post.id, post.publish_time.timestamp())
+                self.broker.set(post.id, post.publish_time.timestamp())
                 await self.schedule_post(post.id, post.owner_id, post.publish_time)
+            if post.delete_time:
+                self.broker.set(f'delete:{post.id}', post.delete_time.timestamp())
+                await self.schedule_delete(post.id, post.owner_id, post.delete_time)
             return post
         raise HTTPException('Не удалось добавить объект в БД')
 
-    async def delete_post(self, post_id):
-        pass
+    async def delete_post(self, post_id, user_id):
+        result = await self.post_repository.delete_post(post_id, user_id)
+        if result and post_id in self.timers:
+            self.timers[post_id].cancel()
+
+        if result:
+            self.broker.delete(post_id)
+            self.broker.zrem('post_schedule', post_id)
+        return {'message': 'ok'}
 
     async def update_post(self, post_id: str | ObjectId, post: Post):
         pass
@@ -43,27 +53,41 @@ class PostService:
             return post
         else:
             raise TypeError('Неверный тип')
-    
+
     async def get_posts(self, user_id, posted=False) -> list[Post]:
         return await self.post_repository.get_posts(user_id, posted)
 
     async def schedule_post(self, post_id: str, user_id: str, publish_time: datetime):
         delay = (publish_time - datetime.now(timezone.utc)).total_seconds()
-        print(delay)
         if delay < 0:
             raise HTTPException(status_code=400, detail="Дата публикации указана позже текущей даты")
-        
-        await self.broker.zadd('post_schedule', {post_id: publish_time.timestamp()})
-        await self.broker.expire(post_id, int(delay + 60 * 60))
-        
+
+        self.broker.zadd('post_schedule', {post_id: publish_time.timestamp()})
+        self.broker.expire(post_id, int(delay + 60 * 60))
+
         loop = asyncio.get_event_loop()
-        timer = threading.Timer(delay, lambda: loop.create_task(self.post_publisher.fetch_post_and_send_message(post_id, user_id)))
+        timer = threading.Timer(delay, lambda: loop.create_task(
+            self.post_publisher.fetch_post_and_send_message(post_id, user_id)))
         self.timers[post_id] = timer
+        timer.start()
+
+    async def schedule_delete(self, post_id: str, user_id: str, delete_time: datetime):
+        delay_delete = (delete_time - datetime.now(timezone.utc)).total_seconds()
+        if delay_delete < 0:
+            raise HTTPException(status_code=400, detail="Дата удаления указана позже текущей даты")
+
+        self.broker.expire(f'delete:{post_id}', int(delay_delete + 60 * 60))
+
+        loop = asyncio.get_event_loop()
+        timer = threading.Timer(delay_delete,
+                                lambda: loop.create_task(self.post_publisher.delete_post_from_chats(post_id, user_id)))
+        self.timers[f'delete_{post_id}'] = timer
         timer.start()
 
     async def publish_now(self, post_id: str, user_id: str):
         loop = asyncio.get_event_loop()
-        thread = threading.Thread(target=lambda: loop.create_task(self.post_publisher.fetch_post_and_send_message(post_id, user_id)))
+        thread = threading.Thread(
+            target=lambda: loop.create_task(self.post_publisher.fetch_post_and_send_message(post_id, user_id)))
         self.timers[post_id] = thread
         thread.start()
 
@@ -75,11 +99,16 @@ class PostService:
         for task in scheduled_tasks:
             post_id = task.decode('utf-8')
             post = await self.post_repository.get_post(ObjectId(post_id))
+            user_id = post.owner_id
             if post:
                 publish_time = datetime.fromtimestamp(float(self.broker.get(post_id)), tz=timezone.utc)
-                user_id = post.owner_id
                 await self.schedule_post(post_id, user_id, publish_time)
-        
+                try:
+                    delete_time = datetime.fromtimestamp(float(self.broker.get(f'delete:{post_id}')), tz=timezone.utc)
+                    await self.schedule_delete(post_id, user_id, delete_time)
+                except Exception:
+                    ...
+
 
 class UnSuccessfulDBOperationException(Exception):
     pass
