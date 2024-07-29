ab234441ef6603ba269f54b1e46fc0df9a06ad3e
vfaddey <vfaddey@yandex.ru>
2024-07-29 18:23:08 +0300

    еще одна попытка(((

--- test.py
+++ test.py
@@ -1,4 +1,25 @@
-from telebot import TeleBot
+from telebot.async_telebot import AsyncTeleBot
+import asyncio
+import multiprocessing
 
-bot = TeleBot('6759802763:AAHFgRRSPBlgrhC735yJVB2IjB4pMu6CWCo')
-print(bot.get_chat('@unforgetable_facts'))
+
+bot = AsyncTeleBot('6759802763:AAHFgRRSPBlgrhC735yJVB2IjB4pMu6CWCo')
+
+
+@bot.message_handler(func=lambda message: True)
+async def process(message):
+    await bot.send_message(message.chat.id, message.text)
+
+
+async def main():
+    await bot.infinity_polling()
+
+
+def run_bot():
+    asyncio.run(main())
+
+
+if __name__ == '__main__':
+    process = multiprocessing.Process(target=run_bot)
+    process.start()
+
